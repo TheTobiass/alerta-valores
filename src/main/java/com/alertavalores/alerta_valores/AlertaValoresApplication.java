b1f33f8d5b0fc4d7c54e3645cfbc6ffa30c95663
TheTobiass <reginaldotobias.14@gmail.com>
2025-10-10 18:07:39 -0300

    Inclusão da API Google

---
+++ src/main/java/com/alertavalores/alerta_valores/AlertaValoresApplication.java
@@ -0,0 +1,13 @@
+package com.alertavalores.alerta_valores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertaValoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertaValoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alertavalores/alerta_valores/AlertaValoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alertavalores.alerta_valores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaValoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
